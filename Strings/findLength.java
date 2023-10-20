f120788da324ebf9bc29c760f515337214f5d236
Akshitbansal26367 <akshitbansal08082003@gmail.com>
2023-10-20 10:56:11 +0530

    Added Files

---
+++ Strings/findLength.java
@@ -0,0 +1,10 @@
+import java.lang.String;
+
+class findLength
+{
+	public static void main(String args[])
+	{
+		String a = "Akshit Bansal";
+		System.out.println("The length of string is "+a.length());
+	}
+}
